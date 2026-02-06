b272ec0a040892752d6a7c5f070ce65d8d2ae4b1
ashfaqahmedkhan112200-debug <ashfaqahmedkhan112200@gmail.com>
2026-02-06 21:35:00 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,38 @@
+// Open popup with dynamic data
+document.querySelectorAll(".openPopup").forEach(button => {
+  button.addEventListener("click", function() {
+
+    document.getElementById("popupName").innerText = this.dataset.name;
+    document.getElementById("popupPrice").innerText = this.dataset.price;
+    document.getElementById("popupImg").src = this.dataset.img;
+    document.getElementById("popupDesc").innerText = this.dataset.desc;
+
+    document.getElementById("popup").style.display = "block";
+  });
+});
+
+// Close popup
+document.querySelector(".close-btn").onclick = function() {
+  document.getElementById("popup").style.display = "none";
+};
+
+// Close when clicking outside
+window.onclick = function(e) {
+  if (e.target == document.getElementById("popup")) {
+    document.getElementById("popup").style.display = "none";
+  }
+};
+
+//   order-detail
+
+document.getElementById("orderForm").addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("✅ Your order has been submitted! We will contact you soon.");
+});
+
+
+
+
+
+
+
